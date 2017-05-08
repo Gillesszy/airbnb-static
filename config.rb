eff3fa670363b4dadf5e1e7e2c37e41e1d2d3982
gilles szymusiak <wippikas@hotmail.fr>
2017-05-08 18:23:17 +0200

    fin day

--- config.rb
+++ config.rb
@@ -63,7 +63,9 @@ configure :build do
 
   # Minify Javascript on build
   # activate :minify_javascript
-
+["anne", "seb", "romain"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner => name }, :ignore => true
+end
   # Enable cache buster
   # activate :asset_hash
 
